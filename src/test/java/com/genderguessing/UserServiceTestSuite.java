68c35243aefbe6c31e5d0e0d0a858e6b297ca98f
Magdalena <magdalena.horbaczyk@gmail.com>
2021-03-07 22:42:51 +0100

    Fixing "java.lang.Exception: No runnable methods exception in running JUnits" due to wrong import.

--- src/test/java/com/genderguessing/UserServiceTestSuite.java
+++ src/test/java/com/genderguessing/UserServiceTestSuite.java
@@ -4,7 +4,6 @@ import com.genderguessing.exception.FileReaderException;
 import com.genderguessing.service.UserService;
 import org.junit.*;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
