cfb23f6c4267d242b4d10d5e5b02f5427565cdc8
Magdalena <magdalena.horbaczyk@gmail.com>
2021-03-07 12:11:01 +0100

    Initial commit

---
+++ src/main/java/com/genderguessing/GenderGuessingApplication.java
@@ -0,0 +1,13 @@
+package com.genderguessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenderGuessingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenderGuessingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/genderguessing/GenderGuessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genderguessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenderGuessingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
